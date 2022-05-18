4118eb981563a9e72cd9c057f1959991b451344e
srenuka123 <srenuka065@gmail.com>
2022-05-18 14:48:24 +0000

    my first java prg

---
+++ day1 core java/AddNumbers.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class AddNumbers {
+
+
+	public static void main(String[] args) {
+		
+		int number1 = 10;
+		int number2 = 25;
+		int result = number1 + number2;
+		
+		System.out.println("sum of two numbers "+ result); {
+
+	}
+
+}
+}
