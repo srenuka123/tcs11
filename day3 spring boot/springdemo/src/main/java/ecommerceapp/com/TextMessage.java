1be2a29c26ed71076b74f549b5241c571e9088c0
srenuka123 <srenuka065@gmail.com>
2022-05-20 07:10:35 +0000

    spring boot application

---
+++ day3 spring boot/springdemo/src/main/java/ecommerceapp/com/TextMessage.java
@@ -0,0 +1,21 @@
+package ecommerceapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* @RestController is a convenience annotation for creating Restful controllers.
+ *  It is a specialization of @Component and is autodetected through classpath scanning.
+ *  It adds the @Controller and @ResponseBody annotations.
+ *   It converts the response to JSON or XML.*/
+
+@RestController
+public class MessageApi {
+	// GET
+	@GetMapping(value = "/showmessage") // this is called point
+	String displayMessage() {
+
+		return "hi how are you";
+
+	}
+
+}
