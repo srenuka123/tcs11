3c10fb98d46f11d53827b45da7e18646953fb98b
srenuka123 <srenuka065@gmail.com>
2022-05-29 15:34:39 +0000

    spring post operation

---
+++ day4 spring post operation/Springdemoo1/src/main/java/amazonapp/Springdemoo1Application.java
@@ -0,0 +1,13 @@
+package amazonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdemoo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springdemoo1Application.class, args);
+	}
+
+}
